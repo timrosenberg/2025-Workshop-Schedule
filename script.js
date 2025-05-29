37734f14ff1a0a498a1738b9231eeb6ad4ed90b9
Timothy Rosenberg <timothyrosenberg@gmail.com>
2025-05-28 22:14:49 -0400

    JSON path update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function loadSchedule() {
-  const response = await fetch('2025-Workshop-Schedule/schedule.json');
+  const response = await fetch('./schedule.json');
   const data = await response.json();
 
   const scheduleContainer = document.getElementById('schedule');
